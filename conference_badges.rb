97d21c068147ee44993919ebaa50011c4de02d55
mchoi89 <mchoi89@gmail.com>
2018-11-11 20:32:06 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@
 
 #step 1: need BADGE_MAKER to say "Hello, my name is _."
 #step 2: need BATCH_BADGE_CREATOR for conference speakers array [Edsger, Ada, Charles, Alan, Grace, Linus, and Matz.] as argument & RETURN array badge messages
-#step 3: need ASSIGN_ROOMS (1-7) to take list of speakers and assign 1 room each / list starting at 0 per traditional computer-recognized origin index? 
+#step 3: need ASSIGN_ROOMS (1-7) to take list of speakers and assign 1 room each / does the list need to be starting at 0 per traditional computer-recognized origin index? hmm..
 #step 4: need to output (print?) "Hello, [speaker], you'll be assigned to [room number]!"
 #step 5: need PRINTER to output results of BATCH BADGE CREATOR and then ASSIGN ROOMS (which is array of room assignmets = [ ]? how to print out each assignment / iterate over your array of room assingments in order to "puts" assignment)
 
@@ -29,6 +29,17 @@ end
 def assign_room(conferencespeakers)
   counter = 0 
   conferencespeakers.each do |speakername|
-    counter =+1 < 7 
-    puts 
+    counter = +1 < 7 
+    puts "Hello #{speakername}! You'll be assigned to room #{conferenceroom}!"
+  end
+end
+
+#PRINTER 
+
+def printer (conferencespeakers)
+  batch_badge_creator(conferencespeakers).each do |output|
+    puts output 
+  end
+  
+
 
