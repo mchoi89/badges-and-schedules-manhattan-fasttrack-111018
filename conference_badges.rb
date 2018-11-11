01475b5b5806ab1f460f953c074796c3788da42c
mchoi89 <mchoi89@gmail.com>
2018-11-11 21:07:52 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,13 +5,12 @@
 
 #step 1: need BADGE_MAKER to say "Hello, my name is _."
 #step 2: need BATCH_BADGE_CREATOR for conference speakers array [Edsger, Ada, Charles, Alan, Grace, Linus, and Matz.] as argument & RETURN array badge messages
-#step 3: need ASSIGN_ROOMS (1-7) to take list of speakers and assign 1 room each / does the list need to be starting at 0 per traditional computer-recognized origin index? hmm..
-#step 4: need to output (print?) "Hello, [speaker], you'll be assigned to [room number]!"
+#step 3: need ASSIGN_ROOMS (1-7) to take list of speakers and assign 1 room each / but does the list need to be starting at 0 per traditional 'origin' index? or can i define it 1-7 
+#step 4: need to output (print?) "Hello, [speakername], you'll be assigned to [conferenceroom!"
 #step 5: need PRINTER to output results of BATCH BADGE CREATOR and then ASSIGN ROOMS (which is array of room assignmets = [ ]? how to print out each assignment / iterate over your array of room assingments in order to "puts" assignment)
 
 #BADGE_MAKER *individuals
 
-conferencespeakers = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
 speakername = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 conferenceroom = [1-7]
 
@@ -19,7 +18,9 @@ def badge_maker(speakername)
   puts "Hello, my name is #{speakername}."
 end
 
-#BATCH_BADGE_CREATOR *new array 
+#BATCH_BADGE_CREATOR *create new array of names 
+
+conferencespeakers = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
 
 def batch_badge_creator(conferencespeakers)
   badges = [ ]
@@ -28,10 +29,10 @@ def batch_badge_creator(conferencespeakers)
   end 
 end
 
-#ASSIGN_ROOMS (1-7) *how to make it end at 7???
+#ASSIGN_ROOMS (1-7) *tried each_with_index, but how do i make it end at 7???
 
 def assign_room(conferencespeakers)
-  counter = 0 
+  counter = 0
   conferencespeakers.each_with_index(1-7) do {|speakername, index|}
     counter = +1 
     puts "Hello #{speakername}! You'll be assigned to room #{index}!"
@@ -39,7 +40,7 @@ def assign_room(conferencespeakers)
   end
 end
 
-#PRINTER *both names and rooms here, pull from top 
+#PRINTER *both names and rooms here, pull from top arrays?
 
 def printer (conferencespeakers)
   batch_badge_creator(conferencespeakers).each do |speakername|
