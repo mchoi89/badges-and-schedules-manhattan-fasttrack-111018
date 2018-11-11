c24761676eab01cbb095df51af227902fe47bf0c
mchoi89 <mchoi89@gmail.com>
2018-11-11 20:46:42 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -29,7 +29,7 @@ end
 
 def assign_room(conferencespeakers)
   counter = 0 
-  conferencespeakers.each_with_index do {|speakername, index|}
+  conferencespeakers.each_with_index(1) do {|speakername, index|}
     counter = +1 
     puts "Hello #{speakername}! You'll be assigned to room #{index}!"
   end
